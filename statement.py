668a0e9071630a273b13922875c6529561ac00f2
innjuun <sroo2315@gmail.com>
2021-03-20 16:49:43 +0900

    add amount

--- statement.py
+++ statement.py
@@ -3,9 +3,25 @@ def statement(invoice, plays):
     def play_for(a_performance):
         return plays[a_performance['play_id']]
 
+    def amount_for(a_performance):
+        result = 0
+        if a_performance['play']['type'] == "tragedy":
+            result = 40000
+            if a_performance['audience'] > 20:
+                result += 1000 * (a_performance['audience'] - 30)
+        elif a_performance['play']['type'] == "comedy":
+            result = 30000
+            if a_performance['audience'] > 20:
+                result += 10000 + 500 * (a_performance['audience'] - 20)
+            result += 300 * a_performance['audience']
+        else:
+            raise Exception(f"Unknown type: {a_performance['play']['type']}")
+        return result
+
     def enrich_performance(a_performance):
         result = dict(a_performance)
-        result['play'] = play_for(a_performance)
+        result['play'] = play_for(result)
+        result['amount'] = amount_for(result)
         return result
 
     statement_data = {}
@@ -23,25 +39,11 @@ def render_plain_text(data, plays):
             result += a_performance['audience'] // 5
         return result
 
-    def amount_for(a_performance):
-        result = 0
-        if a_performance['play']['type'] == "tragedy":
-            result = 40000
-            if a_performance['audience'] > 20:
-                result += 1000 * (a_performance['audience'] - 30)
-        elif a_performance['play']['type'] == "comedy":
-            result = 30000
-            if a_performance['audience'] > 20:
-                result += 10000 + 500 * (a_performance['audience'] - 20)
-            result += 300 * a_performance['audience']
-        else:
-            raise Exception(f"Unknown type: {a_performance['play']['type']}")
-        return result
 
     def total_amount():
         result = 0
         for perf in data['performances']:
-            result += amount_for(perf)
+            result += perf['amount']
         return result
 
     def total_volume_credits():
@@ -53,7 +55,7 @@ def render_plain_text(data, plays):
     result = f"청구 내역 (고객명: {data['customer']})"
 
     for perf in data['performances']:
-        result += f"{perf['play']['name']}: {amount_for(perf)//100} ({perf['audience']}석)\n"
+        result += f"{perf['play']['name']}: {perf['amount']//100} ({perf['audience']}석)\n"
 
     result += f"총액: {total_amount()//100}\n"
     result += f'적립 포인트: {total_volume_credits()}점\n'
