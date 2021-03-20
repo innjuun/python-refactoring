245e12d66a4ac5a20303df3e1393280367980fe1
innjuun <sroo2315@gmail.com>
2021-03-20 17:14:10 +0900

    add function, test code

--- statement.py
+++ statement.py
@@ -1,4 +1,12 @@
 def statement(invoice, plays):
+    return render_plain_text(create_statement_data(invoice, plays))
+
+
+def html_statement(invoice, plays):
+    return render_html(create_statement_data(invoice, plays))
+
+
+def create_statement_data(invoice, plays):
 
     def play_for(a_performance):
         return plays[a_performance['play_id']]
@@ -44,16 +52,15 @@ def statement(invoice, plays):
         result['volume_credits'] = volume_credits_for(result)
         return result
 
-    statement_data = {}
-    statement_data['customer'] = invoice['customer']
-    statement_data['performances'] = [enrich_performance(performance) for performance in invoice['performances']]
-    statement_data['total_amount'] = total_amount(statement_data)
-    statement_data['total_volume_credits'] = total_volume_credits(statement_data)
-    return render_plain_text(statement_data, plays)
+    result = {}
+    result['customer'] = invoice['customer']
+    result['performances'] = [enrich_performance(performance) for performance in invoice['performances']]
+    result['total_amount'] = total_amount(result)
+    result['total_volume_credits'] = total_volume_credits(result)
 
+    return result
 
-def render_plain_text(data, plays):
-
+def render_plain_text(data):
     result = f"청구 내역 (고객명: {data['customer']})"
 
     for perf in data['performances']:
@@ -61,4 +68,16 @@ def render_plain_text(data, plays):
 
     result += f"총액: {data['total_amount']//100}\n"
     result += f"적립 포인트: {data['total_volume_credits']}점\n"
+    return result
+
+def render_html(data):
+    result = f"<h1>청구 내역 (고객명: {data['customer']}</h1>"
+    result += "<table>\n"
+    result += "<tr><th>연극</th><th>좌석 수</th><th>금액</th></tr>"
+    for perf in data['performances']:
+        result += f"<tr><td>{perf['play']['name']}</td><td>({perf['audience']}석)</td>"
+        result += f"<td>{perf['amount']}</td></tr>"
+    result += "</trble>\n"
+    result += f"<p>총액: <em>{data['total_amount']}</em></p>\n"
+    result += f"<p>적립 포인트: <em>{data['total_volume_credits']}</em>점</p>\n"
     return result
--- tests.py
+++ tests.py
@@ -1,5 +1,5 @@
 import unittest
-from statement import statement
+from statement import html_statement, statement
 class StatementTest(unittest.TestCase):
 
     def setUp(self):
@@ -30,9 +30,16 @@ class StatementTest(unittest.TestCase):
 
     def test_statement(self):
         result = statement(self.invoices[0], self.plays)
-
+        expected = '청구 내역 (고객명: BigCo)Hamlet: 650 (55석)\nAs you like it: 580 (35석)\nOthello: 500 (40석)\n총액: 1730\n적립 포인트: 47점\n'
         self.assertEqual(
-            result, '청구 내역 (고객명: BigCo)Hamlet: 650 (55석)\nAs you like it: 580 (35석)\nOthello: 500 (40석)\n총액: 1730\n적립 포인트: 47점\n'
+            result, expected
+        )
+
+    def test_render_html(self):
+        result = html_statement(self.invoices[0], self.plays)
+        expected = '<h1>청구 내역 (고객명: BigCo</h1><table>\n<tr><th>연극</th><th>좌석 수</th><th>금액</th></tr><tr><td>Hamlet</td><td>(55석)</td><td>65000</td></tr><tr><td>As you like it</td><td>(35석)</td><td>58000</td></tr><tr><td>Othello</td><td>(40석)</td><td>50000</td></tr></trble>\n<p>총액: <em>173000</em></p>\n<p>적립 포인트: <em>47</em>점</p>\n'
+        self.assertEqual(
+            result, expected
         )
 
 if __name__ == '__main__':
